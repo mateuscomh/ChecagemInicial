d75760b6d126f33ce0231d18d6a50e7c39a7871c
Djangoo <60662558+mateuscomh@users.noreply.github.com>
2020-04-07 10:10:19 -0300

    Branch Centos Smart

--- inicializa.sh
+++ inicializa.sh
@@ -6,8 +6,8 @@
 #       REQUISITOS: 
 #       - OBRIGATÓRIO Script seja colocado na inicialização do sistema em /etc/rc.d/init.d/ChekinInicial.sh
 #       - OBRIGATÓRIO caminho de montagem em /mnt para discos e unidades de rede.
-#       HOMOLOGADO: FEDORA 30
-#       VERSAO:  0.2
+#       HOMOLOGADO: CENTOS6
+#       VERSAO:  0.3
 #       CRIADO:  04/04/2020
 #	AUTOR: Matheus Martins
 #       REVISAO:  ---
@@ -18,6 +18,7 @@
 #       - Implementado validação do status do asterisk caso houver no servidor
 #       - Adicionada função de validação de swap e memória livre/em uso
 #       - Adicionadas condições de case e ajustes em tests if
+#       - Nova branch para homologação em CENTOS6
 #=============================================================================================
 
 
@@ -34,11 +35,13 @@ ASTERISK=0
 POSTGRESQL=0
 AUTOMACAO=0 #pendente
 
-
 #VARIAVEIS
-particao="/dev/mapper/fedora_localhost--live-root" 
+#particao="/dev/mapper/fedora_localhost--live-root" 
+particao="/"
 #Informar o caminho da distribuição a qual for utilizar
-#Validado inicialmente no fedora 30, para o centos6 o caminho vai mudar
+#Validado inicialmente no fedora 30 o caminho particao com LVM é /dev/mapper/fedora_localhost--live-root
+#Em CentOS validar o caminho absoluto
+#Validar em discos virtuais 
 
 #CORES
 padrao="\033[0;0m"
@@ -48,7 +51,15 @@ verde="\033[0;32m"
 negrito="\033[;1m"
 azul="\033[1;94m"
 
-#validar a possibilidade de reduzir ifs com case
+echo -e "ATENÇÃO!!!
+￼VALIDAÇÃO INICIAL DE SISTEMA PADRONIZADA:
+￼NÃO IMPLICA NA OBSERVANCIA USUAL DAS DEMAIS APLICAÇÕES E TESTES.
+￼ ____  _   ___  __
+￼/ ___|| \ | \ \/ /
+￼\___ \|  \| |\  / 
+￼ ___) | |\  |/  \ 
+￼|____/|_| \_/_/\_\\
+￼"
 
 if [[ "$ENABLESCRIPT" -eq "0" ]]; then
     echo "exiting"
@@ -56,16 +67,17 @@ if [[ "$ENABLESCRIPT" -eq "0" ]]; then
 fi
 
 check_system(){
-    case $SYSTEM in 
+    case $SYSTEM in
     0)
     ;;
     1)  #SISTEMA OPERACIONAL
-        echo -e "VERSAO DE SISTEMA $azul$(cat /etc/os-release | grep -i pretty |  cut -d "=" -f2)""$padrao"
+            #echo -e "VERSAO DE SISTEMA: $azul$(cat /etc/os-release | grep -i pretty |  cut -d "=" -f2)""$padrao"
+        echo -e "VERSAO DE SISTEMA: $azul$(cat /etc/redhat-release)""$padrao"
         #HORARIO
         echo -e "UTC:           $negrito$(date '+%Z  %d/%m/%Y  %H:%M:%S')""$padrao"
         #UPTIME
         uptime=$(uptime | awk '{print$3,$4}' |sed 's/,//g')
-        echo -e "UPTIME DE SISTEMA :         $negrito$uptime ""$padrao users" 
+        echo -e "UPTIME DE SISTEMA :         $negrito$uptime "" $(w | grep users | awk '{print $6}') $padrao users" 
 
         #DISCO
         percentdisco=$(df -h "$particao" |sed -n '2p' | awk '{print $5}'| grep -v Use |sed 's/%//g')
@@ -96,7 +108,7 @@ check_system(){
         elif [[ "$wa10" -ge "21" ]] && [[ "$wa10" -le "49" ]]; then
             echo -e "IOWAIT (wa)           : $amarelo$wa""$padrao"
         elif [[ "$wa10" -ge "50" ]]; then
-            echo -e "IOWAIT (wa)           : $vermelho$wa""$padrao"
+                    echo -e "IOWAIT (wa)           : $vermelho$wa""$padrao"
         fi
 
         #CPU
@@ -104,20 +116,20 @@ check_system(){
         load=$(cat /proc/loadavg | cut -d ' ' -f 1)
         #Multiplo de 100 para comparar somente inteiros
         load100=$(cat /proc/loadavg | cut -d ' ' -f 1| sed 's/\.//')
-        if [[ "$load100" -le "$(($threads*50))" ]]; then
+        if [[ "$load100" -le $(($threads*50)) ]]; then
         #Ajuste de formatação da quantidade de threads quando tem 1 digito
             if [[ "$threads" -le "9" ]]; then
             echo -e "CPU LOAD $threads"" NUCLEOS          : $verde$load""$padrao"
         else
         echo -e "CPU LOAD $threads""N          : $verde$load""$padrao"
         fi
-        elif [[ "$load100" -ge "$(($threads*50))" ]] && [[ "$load100" -le "$(($threads*74))" ]]; then
+        elif [[ "$load100" -ge $(($threads*50)) ]] && [[ "$load100" -le "$(($threads*74))" ]]; then
         if [[ "$threads" -le "9" ]]; then
                 echo -e "CPU LOAD ""$threads           : $amarelo$load""$padrao"
             else
         echo -e "CPU LOAD $threads"" NUCLEOS          : $amarelo$load""$padrao"
         fi
-        elif [[ "$load100" -ge "$(($threads*75))" ]]; then
+        elif [[ "$load100" -ge $(($threads*75)) ]]; then
         if [[ "$threads" -le "9" ]]; then
         echo -e "CPU LOAD ""$threads           : $vermelho$load""$padrao"
             else
@@ -125,16 +137,16 @@ check_system(){
             fi
         fi
         #echo -e "CPU THREADS           : $threads"
-        
-        #MEMORY
-        totalmem="$(free --mega | grep -i "mem" | awk '{print  $2}')"
-        usadamem="$(free --mega | grep -i "mem" | awk '{print  $3}')"
-        percentmem="$(bc<<<"scale=0;$usadamem*100/$totalmem")"
-        totalswap="$(free --mega | grep -i "swap" | awk '{print  $2}')"
-        usadaswap="$(free --mega | grep -i "swap" | awk '{print  $3}')"
-        percentswap="$(bc<<<"scale=0;$usadaswap*100/$totalswap")"
 
-        echo -e  "MEMORIA TOTAL: $negrito$(bc<<<"scale=2;$totalmem/1000")GB" "$padrao"
+        #MEMORY
+        totalmem="$(free -m | grep -i "mem" | awk '{print  $2}')"
+        usadamem="$(free -m | grep -i "mem" | awk '{print  $3}')"
+        percentmem="$(bc<<<"scale=0;$usadamem*100/$totalmem")"
+        totalswap="$(free -m | grep -i "swap" | awk '{print  $2}')"
+        usadaswap="$(free -m | grep -i "swap" | awk '{print  $3}')"
+        percentswap="$(bc<<<"scale=0;$usadaswap*100/$totalswap")"
+        
+                echo -e  "MEMORIA TOTAL: $negrito$(bc<<<"scale=2;$totalmem/1000")GB" "$padrao"
             if [[ "$percentmem" -le "50" ]]; then
                 echo -e "MEMÓRIA EM USO $negrito$usadamem MB $verde$percentmem%" "$padrao"
             elif [[ "$percentmem" -ge "51" ]] && [[ "$percentmem" -le "75" ]]; then
@@ -158,10 +170,10 @@ check_system(){
 }
 
 check_asterisk(){
-    case $ASTERISK in 
-    0) 
+    case $ASTERISK in
+    0)
     ;;
-    1)  if [[ -d "/etc/asterisk/" ]]; then
+    1)  if [[ -f "/etc/init.d/asterisk" ]]; then
             if ps ax | grep -v grep | grep asterisk > /dev/null; then
                 echo -e "$verde Asterisk Executando!""$padrao"
             else
@@ -170,7 +182,7 @@ check_asterisk(){
         else
             echo -e "$amarelo Serviço Asterisk não localizado!!""$padrao"
         fi
-    ;;
+            ;;
     *) echo -e "Opção desconhecida no script nas variáveis em SYSTEM. Utilize expressões 0 para desabilitar ou 1 para habilitar"
     ;;
     esac
@@ -179,9 +191,9 @@ check_db(){
     case $POSTGRESQL in
     0)
     ;;
-    1) if [ -d "/etc/postgresql/" ]; then
-                if ps ax | grep -v grep | grep postgresql > /dev/null; then
-                
+    1) if [ -f "/etc/init.d/postgresql" ]; then
+                if ps ax | grep -v grep | grep postgres > /dev/null; then
+
                         echo -e "$verde postgreSQL Executando!""$padrao"
                 else
                         echo -e "$vermelho PostgreSQL Parado!!!""$padrao"
@@ -196,14 +208,20 @@ check_db(){
 }
 
 #check_automacao(){
-#em andamento    
+#em andamento
+#msg para print
+#echo -e "$amarelo Serviço Automacao não localizado!!""$padrao"
 #}
+
 
 
 ###########
 
 check_system
 check_db
+#check_automacao
 check_asterisk
 
-exit 0
+#Se colocar no etc/profile, exit0 não pode conter no script
+#exit 0
+        
